63afc292223052f21e1fa871554c0d7cac917993
Arthur Bueno Silva <arthurbueno1395171@hotmail.com>
2024-11-08 20:36:42 -0300

    :sparkles: New custom exceptions

--- hazbin_hotel/src/exceptions/__init__.py
+++ hazbin_hotel/src/exceptions/__init__.py
@@ -1,2 +1,18 @@
 class InvalidRoomType(Exception):
     pass
+
+
+class RoomTypeNotAvailable(Exception):
+    pass
+
+
+class RoomNotAvailable(Exception):
+    pass
+
+
+class RoomHasSchedule(Exception):
+    pass
+
+
+class ScheduleCannotBeOverwritten(Exception):
+    pass
